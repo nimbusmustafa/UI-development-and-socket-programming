1ac4a2734d8c0681caf552189fdf7dc59b9e3af4
Mustafa Haji <117943931+nimbusmustafa@users.noreply.github.com>
2023-09-22 13:36:08 +0530

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mustafa/socket_programming/src
-# Build directory: /home/mustafa/socket_programming/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("gui_socket")
